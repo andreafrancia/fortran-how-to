676bec2213796ef0da1e0d307ae7ba652a910230
Andrea Francia <andrea@andreafrancia.it>
2012-12-04 23:27:22 +0100

    A whole new dir for left_align

--- left_align/left_align.f90
+++ left_align/left_align.f90
@@ -1,3 +1,8 @@
+double precision :: a = 5.
+character* 255 left_align
+write(*, '(a)') left_align('(f10.5)', a)
+end
+
 character *255 function left_align(format, number)
     character(len=255) buffer
     character(len=*) format
@@ -5,12 +10,4 @@ character *255 function left_align(format, number)
     write(buffer, format) number
     left_align=adjustl(buffer)
 end function
-
-program pippo
-    double precision w 
-    character* 255 left_align
-    w = 9.9283982
-    write(*, '(f10.5)') w
-    write(*, '(a)') left_align('(f10.5)', w)
-end program
 
